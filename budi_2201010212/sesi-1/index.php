463524fb8fe0b2151924092ebd8713c441c03472
shadowlotus <115934671+shadowlotus@users.noreply.github.com>
2023-03-15 09:49:37 +0800

    Add files via upload

---
+++ budi_2201010212/sesi-1/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Sesi 1</title>
+</head>
+<body>
+    
+    <h3>Latihan Web Programming</h3>
+
+    <script>
+
+        document.write("Latihan Web Programming - dg JavaScript");
+
+    </script>
+
+    <br>
+
+<?php
+    echo "Latihan Web Programming - dg PHP";
+?>
+
+</body>
+</html>
